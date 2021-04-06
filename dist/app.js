572716b767030f8a56f56503382704e9d799ea5e
John Chan <johnchancontact@gmail.com>
2021-04-06 13:57:09 -0400

    Initial Commit

---
+++ dist/app.js
@@ -0,0 +1,7 @@
+import express from 'express';
+const app = express();
+app.get('/', (req, res) => {
+    res.send('Hello');
+});
+app.listen(5000, () => console.log('Server running'));
+//# sourceMappingURL=app.js.map
